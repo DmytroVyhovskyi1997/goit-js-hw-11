97f372309005507575ee8d0c5de5bad2ec77c1a5
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-01-31 18:58:58 +0200

    update

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
