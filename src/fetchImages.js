2672fc30ec9e79ccf48354bc6ab720c0bfc6a5f2
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-02-01 23:01:56 +0200

    update

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,18 +1,24 @@
 const  API_KEY = '33274070-f9f176f3b3b7b71ec712df31b';
 
-export const fetchImages = async (name, page = 1) => {
-  return await(
-    `https://pixabay.com/api/?key=${API_KEY}&q=${name}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${page}`
-  )
-    .then(async response => {
-      if (!response.ok) {
+// export const fetchImages = async (name, page = 1) => {
+//   return await(
+//     `https://pixabay.com/api/?key=${API_KEY}&q=${name}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${page}`
+//   )
+//     .then(async response => {
+//       if (!response.ok) {
       
-        throw new Error(response.statusText);
-      }
-      return await response.json();
-    })
-};
+//         throw new Error(response.statusText);
+//       }
+//       return await response.json();
+//     })
+// };
 // async function fetchImages (name, page =1){
 //   let res = axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${name}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${page}`)
 //   return await res;
 // }
+export const fetchImages = async (name, page = 1) => {
+  const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${name}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${page}`);
+  const pages = await response.json();
+  return pages;
+};
+
