39be650262a0a5b05d3fe722cc65d19ee05038e0
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-02-01 22:44:32 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import './css/styles.css';
 import { fetchImages } from './fetchImages';
 import Lodash from 'lodash.debounce';
 import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const DEBOUNCE_DELAY = 300;
 
