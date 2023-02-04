5b3086fd567e8f13fda44eb119266d4fc8de4319
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-02-04 13:57:02 +0200

    update renderCountry

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import { fetchImages } from './fetchImages';
+import {renderCountry} from './renderCountry'
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
