7981e2243d4ed83afb29be8bb6d5c8babb183ecc
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-02-04 14:02:42 +0200

    udate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './css/styles.css';
 import { fetchImages } from './fetchImages';
-import {renderCountry} from './renderCountry';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -62,7 +61,33 @@ function clickBtn(){
     });
 }
 
-
+function renderCountry(users) {
+  const markup = users
+    .map((image) => {
+      return `<div class="photo-card">
+      <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width='200'/>
+      <div class="info">
+      <p class="info-item">
+<b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
+</p>
+       <p class="info-item">
+           <b>Views</b> <span class="info-item-api">${image.views}</span>  
+       </p>
+       <p class="info-item">
+           <b>Comments</b> <span class="info-item-api">${image.comments}</span>  
+       </p>
+       <p class="info-item">
+           <b>Downloads</b> <span class="info-item-api">${image.downloads}</span> 
+       </p>
+    </div>
+      </a>
+    
+    </div>
+    `;
+    })
+    .join("");
+    return gallery.insertAdjacentHTML('beforeend', markup)
+}
 
 
 
